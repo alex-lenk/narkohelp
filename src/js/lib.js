941e0857d72ac91032dea135617e805fa6fb42ac
artlenk.ru@gmail.com <df5ukoi7ht>
2017-03-20 11:03:56 +0300

    Removed unnecessary code

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,7 +1,3 @@
 //= lib/jquery.fancybox.min.js
 //= ../../bower_components/slick-carousel/slick/slick.min.js
 //= lib/jquery.inputmask.bundle.min.js
-
-// = lib/bootstrap.min.js
-// = ../../bower_components/slick-carousel/slick/slick.min.js
-// = lib/jquery.inputmask.bundle.min.js
